1944da6e73eedd091051dc2ee7e713519c3af6c3
Deekor666 <deekor666@gmail.com>
2020-07-21 11:04:32 +0700

    New: Start frontend testing, install vuetify.

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,48 +1,30 @@
 <template>
-  <div class="hello row">
-    <h1>{{ msg }}</h1>
-    <div class="col">
-      <ul class="list-group">
-        <li
-          class="list-group-item"
-          v-for="(tutorial, index) in tutorials"
-          :key="index"
-        >
-          {{tutorial.title}}
-        </li>
-      </ul>
-    </div>
-    <h2>{{ tutorials }}</h2>
+  <div id="app">
+    <v-btn v-on:click="show = !show">
+      show
+    </v-btn>
+    <transition name="fade">
+      <h2 class="text-h2" v-if="show">hello</h2>
+    </transition>
   </div>
 </template>
-
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import ClassTestApi from '../services/testApi';
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
-  private tutorials: any = [];
 
-  retrieveTutorials() {
-    ClassTestApi.getAll()
-      .then((response) => {
-        this.tutorials = response.data;
-        console.log(response.data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-  mounted() {
-    this.retrieveTutorials();
+  data() {
+    return {
+      show: true
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style lang="scss">
 h3 {
   margin: 40px 0 0;
 }
