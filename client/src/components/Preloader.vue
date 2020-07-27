95074f6c534c94a73546c0c7eb2de8e2a64b287c
Deekor666 <deekor666@gmail.com>
2020-07-27 17:59:28 +0700

    .

--- client/src/components/Preloader.vue
+++ client/src/components/Preloader.vue
@@ -1,32 +1,38 @@
 <template lang="pug">
     .preloader
-        div.start-button(@click='sayHello') {{this.tap ? hello : ''}}
+        div.start-button(@click='sayHello' v-bind:class="{ 'button-off': this.tap }") {{this.tap ? hello : ''}}
         .block-top
-            .block-top-1.block-top-group
-                .border-line-top
-            .block-top-2.block-top-group
-                .border-line-top
-            .block-top-3.block-top-group
-                .border-line-top
-            .block-top-4.block-top-group
-                .border-line-top
-            .block-top-5.block-top-group
-                .border-line-top
-            .block-top-6.block-top-group
-                .border-line-top
-            .block-top-7.block-top-group
-                .border-line-top
-            .block-top-8.block-top-group
-                .border-line-top
+            .block-top-1.block-top-group(v-bind:class="{ 'block-top-up': this.tap }")
+                .border-line-top(v-bind:class="{ 'border-line-top-off': this.tap }")
+            .block-top-2.block-top-group(v-bind:class="{ 'block-top-up': this.tap }")
+                .top-letter D
+                .border-line-top(v-bind:class="{ 'border-line-top-off': this.tap }")
+            .block-top-3.block-top-group(v-bind:class="{ 'block-top-up': this.tap }")
+                .top-letter E
+                .border-line-top(v-bind:class="{ 'border-line-top-off': this.tap }")
+            .block-top-4.block-top-group(v-bind:class="{ 'block-top-up': this.tap }")
+                .top-letter E
+                .border-line-top(v-bind:class="{ 'border-line-top-off': this.tap }")
+            .block-top-5.block-top-group(v-bind:class="{ 'block-top-up': this.tap }")
+                .top-letter K
+                .border-line-top(v-bind:class="{ 'border-line-top-off': this.tap }")
+            .block-top-6.block-top-group(v-bind:class="{ 'block-top-up': this.tap }")
+                .top-letter O
+                .border-line-top(v-bind:class="{ 'border-line-top-off': this.tap }")
+            .block-top-7.block-top-group(v-bind:class="{ 'block-top-up': this.tap }")
+                .top-letter R
+                .border-line-top(v-bind:class="{ 'border-line-top-off': this.tap }")
+            .block-top-8.block-top-group(v-bind:class="{ 'block-top-up': this.tap }")
+                .border-line-top(v-bind:class="{ 'border-line-top-off': this.tap }")
         .block-bottom
-            .block-bottom-1.block-bottom-group
-            .block-bottom-2.block-bottom-group
-            .block-bottom-3.block-bottom-group
-            .block-bottom-4.block-bottom-group
-            .block-bottom-5.block-bottom-group
-            .block-bottom-6.block-bottom-group
-            .block-bottom-7.block-bottom-group
-            .block-bottom-8.block-bottom-group
+            .block-bottom-1.block-bottom-group(v-bind:class="{ 'block-bottom-down': this.tap }")
+            .block-bottom-2.block-bottom-group(v-bind:class="{ 'block-bottom-down': this.tap }")
+            .block-bottom-3.block-bottom-group(v-bind:class="{ 'block-bottom-down': this.tap }")
+            .block-bottom-4.block-bottom-group(v-bind:class="{ 'block-bottom-down': this.tap }")
+            .block-bottom-5.block-bottom-group(v-bind:class="{ 'block-bottom-down': this.tap }")
+            .block-bottom-6.block-bottom-group(v-bind:class="{ 'block-bottom-down': this.tap }")
+            .block-bottom-7.block-bottom-group(v-bind:class="{ 'block-bottom-down': this.tap }")
+            .block-bottom-8.block-bottom-group(v-bind:class="{ 'block-bottom-down': this.tap }")
 
 </template>
 
@@ -39,7 +45,6 @@
         public hello: string = 'Hello';
         sayHello() {
             this.tap = true;
-            // начать анимацию падения шторок
             // подать знак родителю, что надо из темноты вывести основную страницу
         }
     }
@@ -47,12 +52,24 @@
 
 <style lang="scss">
     .preloader {
-        //   z-index: -10;
+        .top-letter {
+            position: relative;
+            top: 50%;
+            left: 50%;
+            display: inline;
+            font-size: 30px;
+            font-family: "Roboto", sans-serif;
+            color: wheat;
+        }
+
         width: 100%;
         height: 100%;
-        background-color: rgba(0, 0, 0, 0.7);
         position: fixed;
         .start-button {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: wheat;
             z-index: 2;
             width: 100px;
             height: 100px;
@@ -61,11 +78,14 @@
             position: absolute;
             left: 48%;
             top: 48%;
-            animation: start-button 1s linear;
+            animation: start-button 2s linear;
             animation-iteration-count: 1;
             animation-fill-mode: forwards;
-            animation-delay: 8s;
+            animation-delay: 2s;
 
+        }
+        .button-off {
+            animation: button-off 2s linear;
         }
 
         .block-top {
@@ -74,28 +94,37 @@
             align-items: start;
             width: 100%;
             height: 50%;
-            background-color: black;
 
             .block-top-group {
+                background-color: black;
                 position: relative;
                 width: 20%;
+            }
+
+            .block-top-up {
+                opacity: 1;
+                animation: top-up 2s linear;
+                animation-iteration-count: 1;
+                animation-fill-mode: forwards;
             }
 
             .border-line-top {
                 z-index: 1;
                 background-color: aqua;
                 position: relative;
-                top: 100%;
+                top: 99%;
                 width: 0;
-                height: 2px;
+                height: 1px;
+            }
+
+            .border-line-top-off {
+                display: none;
             }
 
             .block-top-1 {
-                transform: rotateX(45grad);
-
                 height: 40%;
                 .border-line-top {
-                    animation: top-1 1s linear;
+                    animation: top-1 500ms linear;
                     animation-iteration-count: 1;
                     animation-fill-mode: forwards;
                 }
@@ -104,84 +133,88 @@
             .block-top-2 {
                 height: 60%;
                 .border-line-top {
-                    animation: top-1 1s linear;
+                    animation: top-1 500ms linear;
                     animation-iteration-count: 1;
                     animation-fill-mode: forwards;
-                    animation-delay: 1s;
+                    animation-delay: 500ms;
                 }
             }
 
             .block-top-3 {
                 height: 80%;
                 .border-line-top {
-                    animation: top-1 1s linear;
+                    animation: top-1 500ms linear;
                     animation-iteration-count: 1;
                     animation-fill-mode: forwards;
-                    animation-delay: 2s;
+                    animation-delay: 1s;
                 }
             }
             .block-top-4 {
                 height: 100%;
                 .border-line-top {
-                    animation: top-1 1s linear;
+                    animation: top-1 500ms linear;
                     animation-iteration-count: 1;
                     animation-fill-mode: forwards;
-                    animation-delay: 3s;
-
+                    animation-delay: 1.5s;
                 }
-
             }
             .block-top-5 {
                 height: 100%;
                 .border-line-top {
-                    animation: top-1 1s linear;
+                    animation: top-1 500ms linear;
                     animation-iteration-count: 1;
                     animation-fill-mode: forwards;
-                    animation-delay: 4s;
+                    animation-delay: 2s;
                 }
             }
 
             .block-top-6 {
                 height: 80%;
                 .border-line-top {
-                    animation: top-1 1s linear;
+                    animation: top-1 500ms linear;
                     animation-iteration-count: 1;
                     animation-fill-mode: forwards;
-                    animation-delay: 5s;
+                    animation-delay: 2.5s;
                 }
             }
 
             .block-top-7 {
                 height: 60%;
                 .border-line-top {
-                    animation: top-1 1s linear;
+                    animation: top-1 500ms linear;
                     animation-iteration-count: 1;
                     animation-fill-mode: forwards;
-                    animation-delay: 6s;
+                    animation-delay: 3s;
                 }
             }
 
             .block-top-8 {
                 height: 40%;
                 .border-line-top {
-                    animation: top-1 1s linear;
+                    animation: top-1 500ms linear;
                     animation-iteration-count: 1;
                     animation-fill-mode: forwards;
-                    animation-delay: 7s;
+                    animation-delay: 3.5s;
                 }
             }
         }
 
         .block-bottom {
-            z-index: -1;
-            background-color: black;
             display: flex;
             justify-content: start;
             align-items: start;
             width: 100%;
             height: 50%;
 
+            .block-bottom-down {
+                opacity: 1;
+                animation: bottom-down 2s linear;
+                animation-iteration-count: 1;
+                animation-fill-mode: forwards;
+            }
+
             .block-bottom-group {
+                background-color: black;
                 width: 20%;
             }
 
@@ -201,6 +234,18 @@
                 position: relative;
                 top: -20%;
                 height: 120%;
+            }
+
+            .block-bottom-4 {
+                position: relative;
+                top: 0;
+                height: 100%;
+            }
+
+            .block-bottom-5 {
+                position: relative;
+                top: 0;
+                height: 100%;
             }
 
             .block-bottom-6 {
@@ -240,6 +285,22 @@
         to {
             border-radius: 50%;
             opacity: 1;
+        }
+    }
+    @keyframes top-up {
+        to {
+            border-radius: 0 0 50px 50px;
+            height: 0;
+        }
+    }
+    @keyframes bottom-down {
+        to {
+            top: 100%;
+        }
+    }
+    @keyframes button-off {
+        to {
+            opacity: 0;
         }
     }
 </style>
